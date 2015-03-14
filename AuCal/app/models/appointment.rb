e4111bd8aa5c4e4c5da396828df33f494a6d4a44
Matt Matero <matt@matero.me>
2015-03-14 03:07:38 -0400

    lib&app models

--- AuCal/app/models/appointment.rb
+++ AuCal/app/models/appointment.rb
@@ -1,3 +1,10 @@
 class Appointment < ActiveRecord::Base
-	
+	belongs_to :librarian
+	@app = Appointment.all
+
+	def getlibs
+		@app.each do |a|
+			a.librarian.where("email = ", params[:email])
+		end
+	end
 end
